b96225ca85963930cfddb8b5f6d8080df8061a20
Somnath Shekhar <snshekhar@paypal.com>
2017-09-01 15:55:50 -0700

    Code refactor

--- app.py
+++ app.py
@@ -1,9 +1,33 @@
 #!flask/bin/python
-
-from flask import Flask, jsonify, abort, make_response, request, url_for
+from flask import Flask, jsonify, abort, request, make_response, url_for
 from flask_httpauth import HTTPBasicAuth
 
-app = Flask(__name__)
+
+app = Flask(__name__, static_url_path = "")
+auth = HTTPBasicAuth()
+
+@auth.get_password
+def get_password(username):
+    if username == 'snshekhar':
+        return 'flask'
+    return None
+
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify( { 'error': 'Unauthorized access' } ), 403)
+    # return 403 instead of 401 to prevent browsers from displaying the default auth dialog
+
+
+@app.errorhandler(400)
+def not_found(error):
+    return make_response(jsonify( { 'error': 'Bad request' } ), 400)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify( { 'error': 'Not found' } ), 404)
+
 
 tasks = [
     {
@@ -21,30 +45,33 @@ tasks = [
 ]
 
 
-auth = HTTPBasicAuth()
-
-@auth.get_password
-def get_password(username):
-    if username == 'snshekhar':
-        return 'flask'
-    return None
-
-@auth.error_handler
-def unauthorized():
-    return make_response(jsonify({'error': 'Unauthorized access'}), 401)
-
-
 def make_public_task(task):
     new_task = {}
     for field in task:
         if field == 'id':
-            new_task['uri'] = url_for('get_task', task_id=task['id'], _external=True)
+            new_task['uri'] = url_for('get_task', task_id = task['id'], _external = True)
         else:
             new_task[field] = task[field]
     return new_task
 
 
-@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+@app.route('/todo/api/v1.0/tasks', methods = ['GET'])
+@auth.login_required
+def get_tasks():
+    return jsonify( { 'tasks': map(make_public_task, tasks) } )
+
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods = ['GET'])
+@auth.login_required
+def get_task(task_id):
+    task = filter(lambda t: t['id'] == task_id, tasks)
+    if len(task) == 0:
+        abort(404)
+    return jsonify( { 'task': make_public_task(task[0]) } )
+
+
+@app.route('/todo/api/v1.0/tasks', methods = ['POST'])
+@auth.login_required
 def create_task():
     if not request.json or not 'title' in request.json:
         abort(400)
@@ -55,27 +82,13 @@ def create_task():
         'done': False
     }
     tasks.append(task)
-    return jsonify({'task': task}), 201
+    return jsonify( { 'task': make_public_task(task) } ), 201
 
 
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
-def get_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
-        abort(404)
-    return jsonify({'task': task[0]})
-
-
-@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods = ['PUT'])
 @auth.login_required
-def get_tasks():
-    #return jsonify({'tasks': tasks})
-    return jsonify({'tasks': [make_public_task(task) for task in tasks]})
-
-
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
 def update_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
+    task = filter(lambda t: t['id'] == task_id, tasks)
     if len(task) == 0:
         abort(404)
     if not request.json:
@@ -89,22 +102,18 @@ def update_task(task_id):
     task[0]['title'] = request.json.get('title', task[0]['title'])
     task[0]['description'] = request.json.get('description', task[0]['description'])
     task[0]['done'] = request.json.get('done', task[0]['done'])
-    return jsonify({'task': task[0]})
+    return jsonify( { 'task': make_public_task(task[0]) } )
 
 
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods = ['DELETE'])
+@auth.login_required
 def delete_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
+    task = filter(lambda t: t['id'] == task_id, tasks)
     if len(task) == 0:
         abort(404)
     tasks.remove(task[0])
-    return jsonify({'result': True})
-
-
-@app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
+    return jsonify( { 'result': True } )
 
 
 if __name__ == '__main__':
-   app.run(host='0.0.0.0', port=8080, threaded=True, debug=True)
+    app.run(host='0.0.0.0', port=8080, threaded=True, debug=True)
